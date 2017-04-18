828ea9498252d62af4ce2a7a21aa2e7b0ffa7cab
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-18 09:56:41 +0200

    Test method added

---
+++ Test/oddaverage/OddAvgTest.java
@@ -0,0 +1,19 @@
+package oddaverage;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class OddAvgTest {
+
+  @Test
+  static double oddAverage() {
+    List <Integer> testList = new ArrayList<>();
+    testList.add(10);
+    testList.add(15);
+    testList.add(5);
+    assertEquals(10, oddAverage(testList));
+  }
+}
--- src/oddaverage/OddAvg.java
+++ src/oddaverage/OddAvg.java
@@ -1,8 +1,32 @@
 package oddaverage;
 
-/**
- * Created by kispista on 2017.04.18..
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class OddAvg {
 
+  public static void main(String[] args) {
+    List<Integer> originalListOfNumbers = new ArrayList<>();
+    originalListOfNumbers.add(21);
+    originalListOfNumbers.add(31);
+    originalListOfNumbers.add(40);
+    originalListOfNumbers.add(51);
+    System.out.println(oddAverage(originalListOfNumbers));
+  }
+
+  public static double oddAverage(List<Integer> originalList) {
+    List<Integer> temporalyListForOddNumbers = new ArrayList<>();
+    double fullValueOfOddNumbers = 0;
+    double averageValueOfOddNumbers = 0;
+    for (int i = 0; i < originalList.size(); i++) {
+      if (originalList.get(i) % 2 == 1) {
+        temporalyListForOddNumbers.add(originalList.get(i));
+      }
+    }
+    for (int i = 0; i < temporalyListForOddNumbers.size(); i++) {
+      fullValueOfOddNumbers += temporalyListForOddNumbers.get(i);
+    }
+    averageValueOfOddNumbers = fullValueOfOddNumbers / temporalyListForOddNumbers.size();
+    return averageValueOfOddNumbers;
+  }
 }
