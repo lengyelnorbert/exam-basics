ee1ba6926e1c7a1e1fcd169f9e430b3db4c9856f
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-18 12:52:51 +0200

    OddAvg test method fixed

--- Test/oddaverage/OddAvgTest.java
+++ Test/oddaverage/OddAvgTest.java
@@ -1,19 +1,44 @@
 package oddaverage;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
+
 class OddAvgTest {
 
-//  @Test
-//  static double oddAverage() {
-//    List <Integer> testList = new ArrayList<>();
-//    testList.add(10);
-//    testList.add(15);
-//    testList.add(5);
-//    assertEquals(10, oddAverage(testList));
-//  }
+  @Test
+  void oddAverage() {
+    List<Integer> testlist = new ArrayList<>();
+    testlist.add(5);
+    testlist.add(27);
+    testlist.add(13);
+    testlist.add(522);
+    assertEquals(15, OddAvg.oddAverage(testlist));
+  }
+
+  @Test
+  void oddAverageSecondTest() {
+    List<Integer> testlist = new ArrayList<>();
+    testlist.add(5);
+    testlist.add(27);
+    testlist.add(13);
+    testlist.add(922);
+    assertFalse(20 == OddAvg.oddAverage(testlist));
+  }
+
+  @Test
+  void oddAverageThirdTest() {
+    List<Integer> testlist = new ArrayList<>();
+    testlist.add(5);
+    testlist.add(27);
+    testlist.add(13);
+    testlist.add(52);
+    assertTrue(15 == OddAvg.oddAverage(testlist));
+  }
+
+
+
 }
